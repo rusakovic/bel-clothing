5302d493a6563b2d8993a00a01c129208de21bd4
maxrusakovich <maxrusakovich@gmail.com>
2019-09-16 19:59:26 +0300

    added redux, and implemented userReducer and userActions to header and app components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import './App.css';
 
@@ -8,36 +9,32 @@ import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { setCurrentUser } from './redux/user/user.actions';
+
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      currentUser: null
-    }
-  }
 
   // when the user log-in we want to get user information from firebase, and we store it into the state
   // but we should remove this info from the state when the components are Unmount
   unsubscribeFromAuth = null
 
   componentDidMount() {
+    const { setCurrentUser } = this.props;
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
      if (userAuth) {
        const userRef = await createUserProfileDocument(userAuth);
       // with onSnaphot we can see the actual data in our DB document
       // snaphot itself contains the ID of the user. Data() all other info we stored
       userRef.onSnapshot(snapShot => {
-         this.setState({
-           currentUser: {
+         setCurrentUser({
              id: snapShot.id,
              ...snapShot.data()
-           }
+           })
          });
-       })
+       
      } else { // but if the user Logged out we should setState to 'userAuth => null' after logging out 
-      this.setState({ currentUser: userAuth })
+      setCurrentUser(userAuth)
     }
     })
   }
@@ -49,7 +46,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser}/>
+        <Header />
         <Switch>
           <Route exact path='/' component={Homepage} />
           <Route path='/shop' component={ShopPage} />
@@ -60,4 +57,8 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(null, mapDispatchToProps)(App);
---
+++ src/redux/user/user.reducer.js
@@ -0,0 +1,19 @@
+// INITIAL_STATE is the same as this.state in constructor in App.js for expample
+const INITIAL_STATE = {
+  currentUser: null
+}
+
+// state = INITIAL STATE, to set the default value of state
+const userReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case 'SET_CURRENT_USER':
+      return {
+        ...state,
+        currentUser: action.payload
+      }
+    default:
+     return state;
+  }
+}
+
+export default userReducer;
