bd932bf7307b043b86a9144629db1dd902ba7760
maxrusakovich <maxrusakovich@gmail.com>
2019-09-18 17:02:42 +0300

    added checkout page, checkout item component and updated cart to user selectors from reselect

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -7,13 +7,27 @@ const selectCart = state => state.cart;
 export const selectCartItems = createSelector(
   [selectCart],
   (cart) => cart.cartItems
+);
+
+export const selectCartHidden = createSelector(
+  [selectCart],
+  cart => cart.hidden
 )
 
 export const selectCartItemsCount = createSelector(
   [selectCartItems],
   cartItems =>
-  cartItems.reduce(
-    (accumalateQuantity, cartItem) => accumalateQuantity + cartItem.quantity, 
-    0
-  )
+    cartItems.reduce(
+      (accumalateQuantity, cartItem) => accumalateQuantity + cartItem.quantity, 
+      0
+    )
+)
+
+export const selectCartTotal = createSelector(
+  [selectCartItems],
+  cartItems =>
+    cartItems.reduce(
+      (accumalateQuantity, cartItem) => accumalateQuantity + cartItem.quantity * cartItem.price, 
+      0
+    )
 )
