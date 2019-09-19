e8fa9d13799f003faa90b52cfbfc2053c4a3ffb2
maxrusakovich <maxrusakovich@gmail.com>
2019-09-19 17:35:01 +0300

    updating stripe integration and updating our favicon

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import logo from '../../assets/logo.svg';
+
+import { ReactComponent as Logo } from '../../assets/logo.svg';
+
+const StripeCheckoutButton = ({ price }) => {
+  // stripe want to see prices in cents. so, we multiply by 100
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_frIEpaKIw7lECFggqwoOaLp900NzRv1FcB';
+  
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Successful')
+  }
+
+  return (
+    <StripeCheckout 
+      label='Pay Now'
+      name='Belarusian Clothes'
+      billingAddress
+      shippingAddress
+      image={logo}
+      description={`Your total is $${price}`}
+      amout={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  )
+}
+
+export default StripeCheckoutButton;
